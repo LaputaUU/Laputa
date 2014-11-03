7141aaa3f79e82ad2ff359f189ead86a8f42eeee
LaputaUU <1184019804@qq.com>
2014-11-03 23:47:45 +0800

    Stamp
    拍照及截图功能

---
+++ Stamp/src/com/njusoftware/stamp/CropImage.java
@@ -0,0 +1,7 @@
+package com.njusoftware.stamp;
+
+import android.app.Activity;
+
+public class CropImage extends Activity{
+
+}
---
+++ Stamp/src/com/njusoftware/stamp/MainActivity.java
@@ -0,0 +1,213 @@
+package com.njusoftware.stamp;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "MainActivity";
+	Preview preview;
+	Button buttonconfirm;
+	Button buttoncancel;
+	Camera camera;
+	Activity act;
+	Context ctx;
+	Uri uri;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ctx = this;
+		act = this;
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.activity_main);
+
+		buttonconfirm = (Button)findViewById(R.id.btnconfirm);
+		buttoncancel = (Button)findViewById(R.id.btncancel);
+		buttonconfirm.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				photoZoom(uri);
+			}
+		});
+		buttoncancel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				resetCam();
+			}
+		});
+		buttonconfirm.setVisibility(View.INVISIBLE);
+		buttoncancel.setVisibility(View.INVISIBLE);
+		preview = new Preview(this, (SurfaceView)findViewById(R.id.surfaceView));
+		preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		((FrameLayout) findViewById(R.id.layout)).addView(preview);
+		preview.setKeepScreenOn(true);
+
+		preview.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+				preview.setClickable(false);
+			}
+		});
+
+		Toast.makeText(ctx, getString(R.string.take_photo_help), Toast.LENGTH_LONG).show();
+
+	}
+
+	@SuppressLint("NewApi") @Override
+	protected void onResume() {
+		super.onResume();
+		int numCams = Camera.getNumberOfCameras();
+		if(numCams > 0){
+			try{
+				camera = Camera.open(0);
+				camera.startPreview();
+				preview.setCamera(camera);
+			} catch (RuntimeException ex){
+				Toast.makeText(ctx, getString(R.string.camera_not_found), Toast.LENGTH_LONG).show();
+			}
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		if(camera != null) {
+			camera.stopPreview();
+			preview.setCamera(null);
+			camera.release();
+			camera = null;
+		}
+		super.onPause();
+	}
+
+	private void resetCam() {
+		preview.setClickable(true);
+		buttonconfirm.setVisibility(View.INVISIBLE);
+		buttoncancel.setVisibility(View.INVISIBLE);
+		camera.startPreview();
+		preview.setCamera(camera);
+	}
+
+	private void refreshGallery(File file) {
+		Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		mediaScanIntent.setData(Uri.fromFile(file));
+		sendBroadcast(mediaScanIntent);
+	}
+
+	ShutterCallback shutterCallback = new ShutterCallback() {
+		public void onShutter() {
+			//			 Log.d(TAG, "onShutter'd");
+		}
+	};
+
+	PictureCallback rawCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			//			 Log.d(TAG, "onPictureTaken - raw");
+		}
+	};
+
+	PictureCallback jpegCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			new SaveImageTask().execute(data);
+			//resetCam();
+			buttonconfirm.setVisibility(View.VISIBLE);
+			buttoncancel.setVisibility(View.VISIBLE);
+			Log.d(TAG, "onPictureTaken - jpeg");
+		}
+	};
+
+	private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+
+		@Override
+		protected Void doInBackground(byte[]... data) {
+			FileOutputStream outStream = null;
+
+			// Write to SD Card
+			try {
+				File sdCard = Environment.getExternalStorageDirectory();
+				File dir = new File (sdCard.getAbsolutePath() + "/camtest");
+				dir.mkdirs();				
+
+				String fileName = String.format("%d.jpg", System.currentTimeMillis());
+				File outFile = new File(dir, fileName);
+				uri = Uri.fromFile(outFile);
+				outStream = new FileOutputStream(outFile);
+				outStream.write(data[0]);
+				outStream.flush();
+				outStream.close();
+
+				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
+
+				refreshGallery(outFile);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+			}
+			return null;
+		}
+
+	}
+	
+	public void photoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 250);
+        intent.putExtra("outputY", 250);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, 1);
+    }
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bundle extras = data.getExtras();
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    photo.compress(Bitmap.CompressFormat.JPEG, 75, stream);
+                    //preview.setVisibility(View.GONE);
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+}
+}
+
+
